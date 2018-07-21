f28dcd15bac79ae2cf9f2a325d97edf1c1b8d5ca
Gurkenglas <gurkenglas@hotmail.de>
2018-07-21 22:29:50 +0200

    Create HoleTweaker.hs

---
+++ src/Haskell/Ide/Engine/Plugin/HoleTweaker.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Haskell.Ide.Engine.Plugin.Example2 where
+
+import           Control.Monad.IO.Class
+#if __GLASGOW_HASKELL__ < 804
+import           Data.Monoid
+#endif
+import qualified Data.Text                     as T
+import           Haskell.Ide.Engine.MonadTypes
+
+-- ---------------------------------------------------------------------
+
+example2Descriptor :: PluginDescriptor
+example2Descriptor = PluginDescriptor
+  {
+    pluginName = "Hello World"
+  , pluginDesc = "An example of writing an HIE plugin"
+  , pluginCommands =
+      [ PluginCommand "sayHello" "say hello" sayHelloCmd
+      , PluginCommand "sayHelloTo ""say hello to the passed in param" sayHelloToCmd
+      ]
+  }
+
+-- ---------------------------------------------------------------------
+
+sayHelloCmd :: CommandFunc () T.Text
+sayHelloCmd = CmdSync $ \_ -> return (IdeResultOk sayHello)
+
+sayHelloToCmd :: CommandFunc T.Text T.Text
+sayHelloToCmd = CmdSync $ \n -> do
+  r <- liftIO $ sayHelloTo n
+  return $ IdeResultOk r
+
+-- ---------------------------------------------------------------------
+
+sayHello :: T.Text
+sayHello = "hello from ExamplePlugin2"
+
+sayHelloTo :: T.Text -> IO T.Text
+sayHelloTo n = return $ "hello " <> n <> " from ExamplePlugin2"
